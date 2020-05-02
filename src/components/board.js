cab4e889eff5634e6659a92a5d488496ba852e0b
Davi <davimargelo@hotmail.com>
2020-05-02 00:22:11 -0300

    botoes de ação editar e deletar/sair do quadro adicionados + modal para editar

--- src/components/board.js
+++ src/components/board.js
@@ -2,11 +2,21 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
-import React from 'react';
-import { getToken } from '../authentication/auth';
+import React, { useState, useEffect } from 'react';
+import { getToken, getId } from '../authentication/auth';
 import api from '../services/api.js';
 import './board.css';
 import CircularIndeterminate from '../components/loading.js'
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -27,18 +37,57 @@ function ListItemLink(props) {
 export default function Boards() {
     const classess = useStyles();
 
+    const [openEdit, setOpenEdit] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
     const [boards, setBoards] = React.useState('');
     const [breakEl, setBreakEl] = React.useState(false);
+    const [editedBoardName, setEditedBoardName] = React.useState(null);
+    const [editedBoardDescription, setEditedBoardDescription] = React.useState(null);
     const token = getToken();
+    const id = getId();
 
     const setLoadingTrue = () => {
         setLoading(true);
-      }
-    
-      const setLoadingFalse = () => {
+    }
+
+    const setLoadingFalse = () => {
         setLoading(false);
-      }
+    }
+
+    const setEditTrue = () => {
+        setOpenEdit(true);
+    }
+
+    const setEditFalse = () => {
+        setEditedBoardName(null);
+        setEditedBoardDescription(null);
+        setOpenEdit(false);
+    }
+
+    function handleEditClose() {
+
+    }
+
+    function handleEditCloseAndSend(board ) {
+        // if (openEdit && a) {
+        //     setLoadingTrue();
+        //     api.put('/boards/' + board.id,
+        //         {
+        //             name: editedBoardName,
+        //             description: editedBoardDescription
+        //         },
+        //         {
+        //             headers: {
+        //                 Authorization: token  //the token is a variable which holds the token
+        //             }
+        //         }).then(res => {
+        //             setLoadingFalse();
+        //         })
+        //         .catch(err => {
+        //             setLoadingFalse()
+        //         });
+        // }
+    }
 
     async function getBoards() { //paulinho@gmail.com,senha=456456
         setLoadingTrue();
@@ -71,21 +120,91 @@ export default function Boards() {
     return (
         <div className={classess.root}>
             {loading ? <CircularIndeterminate /> :
-                <List component="nav" aria-label="main mailbox folders" className="list">
-                    {boards ? boards.content.map((board) =>
-                        <ListItemLink href={"/boards/" + board.id}>
-                            <ListItemText primary="" className="title" />
-                            <div className="boardDescription">
-                                <strong>Nome: </strong> {board.name}
-                                <br />
-                                <strong>Descrição: </strong>{board.description}
-                                <br />
-                                <strong>Criador do Quadro: </strong>{board.owner.name}
-                            </div>
-                        </ListItemLink>
-                    ) : null}
-                </List>
+                <div>
+                    <div>
+
+                    </div>
+                    <List component="nav" aria-label="main mailbox folders" className="list">
+                        {boards ? boards.content.map((board) =>
+                            <ListItem className="item">
+                                <ListItemLink href={"/boards/" + board.id}>
+                                    <ListItemText primary="" className="title" />
+                                    <div className="boardDescription">
+
+                                        <strong>Nome: </strong> {board.name}
+                                        <br />
+                                        <strong>Descrição: </strong>{board.description}
+                                        <br />
+                                        <strong>Criador do Quadro: </strong>{board.owner.name}
+                                    </div>
+                                </ListItemLink>
+                                <div className="actions">
+                                    {
+                                        Number(id) === board.owner.id ? <IconButton
+                                            variant="contained"
+                                            color="primary"
+                                            aria-label="edit"
+                                            className="editButton"
+                                            onClick={setEditTrue}>
+                                            <EditIcon />
+                                        </IconButton> : null
+                                    }
+
+                                    <IconButton
+                                        variant="contained"
+                                        color="secondary"
+                                        aria-label="delete"
+                                        className="deleteButton">
+                                        <DeleteIcon />
+                                    </IconButton>
+
+                                </div>
+
+                                <Dialog open={openEdit} onClose={handleEditClose} aria-labelledby="form-dialog-title">
+                                    <DialogContent>
+                                        <DialogContentText>
+                                            teste
+                                        </DialogContentText>
+                                        {editedBoardName === null ? setEditedBoardName(board.name) : null}
+                                        {editedBoardDescription === null ? setEditedBoardDescription(board.description) : null}
+                                        <TextField
+                                            autoFocus
+                                            margin="dense"
+                                            id={board.name}
+                                            label="Nome"
+                                            type="text"
+                                            fullWidth
+                                            onChange={event => setEditedBoardName(event.target.value)}
+                                            value={editedBoardName}
+                                        />
+                                        <TextField
+                                            autoFocus
+                                            margin="dense"
+                                            multiline
+                                            rows={6}
+                                            id={board.description}
+                                            label="Descrição"
+                                            type="text"
+                                            fullWidth
+                                            onChange={event => setEditedBoardDescription(event.target.value)}
+                                            value={editedBoardDescription}
+                                        />
+                                    </DialogContent>
+                                    <DialogActions>
+                                        <Button onClick={setEditFalse} color="primary">
+                                            Cancelar
+                                        </Button>
+                                        <Button onClick={handleEditCloseAndSend(board)} color="primary">
+                                            Salvar
+                                        </Button>
+                                    </DialogActions>
+                                </Dialog>
+                            </ListItem>
+                        ) : null}
+                    </List>
+                </div>
             }
+
         </div>
     )
 }
