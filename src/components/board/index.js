e91e13282e0a059186e770f64ec7957803cad436
Davi <davimargelo@hotmail.com>
2020-05-21 20:52:22 -0300

    folder enums created

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -10,7 +10,7 @@ import BoardContext from './context'
 
 import DatatoFeed from '../../board/board_feed';
 
-import backgrounds from "../../backgrounds/backgrounds.js"
+import backgrounds from "../../enums/backgrounds.js"
 
 export default function Board_Content({board_background, data_cards, board_id}){
 
--- src/enums/backgrounds.js
+++ src/enums/backgrounds.js
@@ -10,34 +10,30 @@ import Background_8 from "../assets/img/Background_8.jpg"
 import Background_9 from "../assets/img/Background_9.jpg"
 import Background_10 from "../assets/img/Background_10.jpg"
 
-
-
-export default function getBackgrounds(){
-
-
+export default function getBackgrounds() {
     let backgrounds = [{
-        id:"B0", content: Background_0
+        id: "B0", content: Background_0
     }, {
-        id:"B1", content: Background_1
-    },{
-        id:"B2", content: Background_2
-    },{
-        id:"B3", content: Background_3
-    },{
-        id:"B4", content: Background_4
-    },{
-        id:"B5", content: Background_5
-    },{
-        id:"B6", content: Background_6
-    },{
-        id:"B7", content: Background_7
+        id: "B1", content: Background_1
     }, {
-        id:"B8", content: Background_8
+        id: "B2", content: Background_2
     }, {
-        id:"B9", content: Background_9
+        id: "B3", content: Background_3
     }, {
-        id:"B10", content: Background_10
-    },       
+        id: "B4", content: Background_4
+    }, {
+        id: "B5", content: Background_5
+    }, {
+        id: "B6", content: Background_6
+    }, {
+        id: "B7", content: Background_7
+    }, {
+        id: "B8", content: Background_8
+    }, {
+        id: "B9", content: Background_9
+    }, {
+        id: "B10", content: Background_10
+    },
     ]
 
     return backgrounds;
