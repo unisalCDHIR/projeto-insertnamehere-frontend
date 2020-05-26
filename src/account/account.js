d574e723d58523cdad35c7a687a151a811630211
Caio <caiomoloni99@gmail.com>
2020-05-25 21:34:03 -0300

    user profile

--- src/account/account.js
+++ src/account/account.js
@@ -7,7 +7,7 @@ import Snackbar from '@material-ui/core/Snackbar'
 import FaceIcon from '@material-ui/icons/Face';
 import "../account/account.css"
 import { green } from '@material-ui/core/colors';
-import GlobalStyle from "../styles/global"
+import avatars_data from "../enums/icons"
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -23,6 +23,8 @@ export default function Profile() {
     const [openError, setOpenError] = React.useState(false);
     const [open, setOpen] = React.useState(false);
     const [breakEl, setBreakEl] = React.useState(false);
+    const [userAvatar, setUserAvatar] = React.useState('');
+    const [avatars, setAvatars] = React.useState(avatars_data);
 
     async function getUser() {
         setBreakEl(true);
@@ -32,13 +34,27 @@ export default function Profile() {
             }
         }).then(res => {
             setUser(res.data);
+            setUserAvatar(res.data.avatar);
         })
             .catch(err => {
                 err.response.data ? setError(err.response.data.errors[0].defaultMessage) : setError('ERRO DESCONHECIDO');
                 setOpen(true)
                 setOpenError(true);
+            
             });
     }
+
+    function getIconId(user_avatar){
+        let board_b = "";
+        if(user_avatar.length === 3){
+          board_b = user_avatar[1] + user_avatar[2];
+          console.log(board_b);
+        }
+        else{
+          board_b = user_avatar[1];
+        }
+        return board_b;
+       }
 
     // TODO
     // setInterval(() => {
@@ -60,15 +76,30 @@ export default function Profile() {
     //TODO
     return (
         <>
-            <div id="container">
-                <GlobalStyle/>
-                <FaceIcon style={{ fontSize: 100 }} id="userIcon"> </FaceIcon>
-                <br></br><strong>Welcome {user.name}</strong>
+            {user && userAvatar ? <div id="container">
+            {!userAvatar ?   <div id="userIcon_container">
+                   <FaceIcon style={{ fontSize: 100 }} id="userIcon"> </FaceIcon> 
+                    
+                </div>: <div id="userIcon_container_true">
 
-                <div id="userInfo">
+                        <img src={avatars[getIconId(userAvatar)].content}></img>
+
+                    </div>}
+                    
+                <br/><strong id="welcomeInfo">Bem-vindo {user.name}</strong>
+
+                <div id="user_info_container">
+                    <strong id="yourInfo_title">Suas informações</strong>
+                    <br/><br/>
+                    <strong className="yourInfo">Nome da conta:</strong> <br/> <span className="yourInfo"> {user.name}</span>
+                    <br/>
+                    <strong className="yourInfo">Email da conta:</strong> <br/> <span className="yourInfo"> {user.email}</span>
+
                     
                 </div>
-            </div>
+                
+
+            </div> : null}
 
             
         </>
